f98c54c625819008423fcb2f5e2a7afe70d779e6
nyamazones <47697129+nyamazones@users.noreply.github.com>
2020-12-29 10:56:43 +0900

    Add Get-Worksheet.ps1

---
+++ Get-Worksheet.ps1
@@ -0,0 +1,42 @@
+param ([string] $Path, [string] $SheetName)
+
+if ([string]::IsNullOrEmpty($Path) -or
+    [string]::IsNullOrEmpty($SheetName))
+{
+    exit 1
+}
+
+[string] $sheets = Invoke-Expression ($PSScriptRoot + '\Get-Sheets ' + $Path)
+
+if ($LastExitCode -ne 0)
+{
+    exit 1
+}
+
+foreach ($sheet in $sheets -split "`r`n")
+{
+    [string] $tmpSheetName = $sheet -creplace '^(.*?): .*?$', '$1'
+
+    if (!($tmpSheetName -cmatch "^$SheetName$"))
+    {
+        continue
+    }
+
+    [string] $sheetFilePath = $sheet -creplace '^.*?: (.*?)$', '$1'
+
+    [System.Text.RegularExpressions.Match[]] $mathces = (
+        Get-Content $sheetFilePath -Raw -Encoding UTF8 |
+        Select-String '(?s)<worksheet.*?>.*?</worksheet>' `
+            -CaseSensitive -AllMatches
+    ).Matches
+
+    if ($matches.Length -ne 1)
+    {
+        exit 1
+    }
+
+    $mathces[0].Value
+    exit 0
+}
+
+exit 1
